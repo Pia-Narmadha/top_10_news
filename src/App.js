e18512ecd55c83fbbcc36ef33e2adb6ce35c9f46
Pia-Narmadha <meerajen.narmadha@gmail.com>
2022-10-19 14:26:46 -0400

    added backup news

--- src/App.js
+++ src/App.js
@@ -1,131 +1,129 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import {backupNews} from './backUpNews.js'
 
-class Card extends React.Component{
+class Card extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      isHidden:true,
+      isHidden: true,
     }
   }
-  changeState(){
-    this.setState((prevState)=>({isHidden:!prevState.isHidden}));
+  changeState() {
+    this.setState((prevState) => ({ isHidden: !prevState.isHidden }));
   }
-  
-  render(){
-    return(
-      <div className = "card ">
-        <a href={this.props.news.url} target = "_blank">
-              <img src={this.props.news.urlToImage} alt="News" onError={(e)=>{e.target.src="news_alt.png"}}/>
-              <h1> {this.props.news.title} </h1>
+
+  render() {
+    return (
+      <div className="card ">
+        <a href={this.props.news.url} target="_blank">
+          <img src={this.props.news.urlToImage} alt="News" onError={(e) => { e.target.src = "news_alt.png" }} />
+          <h1> {this.props.news.title} </h1>
         </a>
-        </div>
-      );
-    }
-}
-
-const NewsBoard = function (props){
-  var arr =[];
-  var i =0;
-  useEffect(()=>{
-    if(props.news && props.news.articles) {
-        props.news.articles.forEach(news => {
-        arr.push(<Card news={news} key={i} />)
-        i++;
-      });
-    }
-  }, [props.news])
-  return(
-      <div className ="container">
-        {arr}
       </div>
-    )
-}
-const  SourceButton =function(props){
-
-    return(
-    <button  className="sourceButton" onClick = {() =>{
-                        console.log("onclick:"+props.sourceId);
-                        props.onclickHandler(props.sourceId);
-                        }
-                        }> 
-      {props.sourceName} 
-    </button>
     );
-}
-const SourceBar = function(props){
-  var arr=[];
-  for(var i = 0;i<props.sourceNames.length;i++)
-  {
-    var src = props.sourceNames[i];
-    arr.push(<SourceButton key = {i} sourceName={src} sourceId={i} onclickHandler={props.onclickHandler}/>)
   }
-  return(
+}
+
+const NewsBoard = function (props) {
+  return (
+    <div className="container">
+      {props.news && props.news.articles &&
+        props.news.articles.map(news => {
+          return <Card news={news} key={news.title} />
+        })}
+    </div>
+  )
+}
+const SourceButton = function (props) {
+
+  return (
+    <button className="sourceButton" onClick={() => {
+      console.log("onclick:" + props.sourceId);
+      props.onclickHandler(props.sourceId);
+    }
+    }>
+      {props.sourceName}
+    </button>
+  );
+}
+const SourceBar = function (props) {
+  var arr = [];
+  for (var i = 0; i < props.sourceNames.length; i++) {
+    var src = props.sourceNames[i];
+    arr.push(<SourceButton key={i} sourceName={src} sourceId={i} onclickHandler={props.onclickHandler} />)
+  }
+  return (
     <div className="sourceBar">
       {arr}
     </div>
   )
 }
-const LoadingBar = function(props){
-  return(
-      <div className="loader"></div>
+const LoadingBar = function (props) {
+  return (
+    <div className="loader"></div>
   )
 }
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      sources:["google-news-in","cnn","wired","abc-news"],
-      sourceNames:["Google India","CNN","Wired","ABC News"],
-      currentSource:0,
-      news:{articles:[]},
-      isLoading:true,
+      sources: ["google-news-in", "cnn", "wired", "abc-news"],
+      sourceNames: ["Google India", "CNN", "Wired", "ABC News"],
+      currentSource: 0,
+      news: { articles: [] },
+      isLoading: true,
+      backUpNews: false
     }
-    
+
   }
-  componentDidMount(){
+  componentDidMount() {
     this.changeSource(this.state.currentSource);
   }
-  changeSource(i){
-    this.setState({isLoading:true});
+  changeSource(i) {
+    this.setState({ isLoading: true });
     var source = this.state.sources[i];
-    if(i<this.state.sourceNames.length && i>=0)
-    {
-      console.log('changing source to '+this.state.sources[i]);
-      fetch("https://newsapi.org/v2/top-headlines?sources="+source+"&apiKey=ed12c82e8848449ab32b67fd754bdccc")
-      .then(
-        (response)=>{
-          return response.json();
-        }
-      ).then(
-        (data)=>{
-          console.log(data);
-          this.setState({news:data,currentSource:i,isLoading:false});
-        }
-      )
+    if (i < this.state.sourceNames.length && i >= 0) {
+      console.log('changing source to ' + this.state.sources[i]);
+      let newsApiURL = "https://newsapi.org/v2/top-headlines?sources="+source+"&apiKey=ed12c82e8848449ab32b67fd754bdccc";
+      // let newsApiURL = "http://localhost:8080/apis/news/" + source;
+      fetch(newsApiURL)
+        .then(
+          (response) => {
+            return response.json();
+          }
+        ).then(
+          (data) => {
+            console.log(data);
+            this.setState({ news: data, currentSource: i, isLoading: false });
+          }
+        ).catch(err => {
+          console.log("Error occured", err);
+          this.setState({ news: backupNews, currentSource: i, isLoading: false, backUpNews: true });
+        })
     }
-    else
-    {
-      console.log("Incorrect source number"+i);
+    else {
+      console.log("Incorrect source number" + i);
     }
   }
-  
+
   render() {
     var arr = [];
-    if(this.state.isLoading)
-    {
+    if (this.state.isLoading) {
       arr.push(<SourceBar key={1} sourceNames={this.state.sourceNames} onclickHandler={this.changeSource.bind(this)} />)
-      arr.push(<LoadingBar key={2}/>)
+      arr.push(<LoadingBar key={2} />)
     }
-    else
-    {
+    else {
       arr.push(<SourceBar key={1} sourceNames={this.state.sourceNames} onclickHandler={this.changeSource.bind(this)} />)
+      this.state.backUpNews && arr.push(<p>Showing old saved news due to technical issues.</p>)
       arr.push(<NewsBoard key={2} news={this.state.news} />)
-    } 
+    }
     return (
       <div className="App">
-                {arr}
+        
+        {arr}
+        
       </div>
     );
   }
---
+++ src/backUpNews.js
@@ -0,0 +1,123 @@
+export const backupNews = {
+    "status":"ok",
+    "totalResults":9,
+    "articles":[
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":"Abhishek Chakraborty",
+          "title":"'I have made a mistake': UK Home Secretary Suella Braverman quits Liz Truss Cabinet",
+          "description":"UK Home Secretary Suella Braverman has tendered her resignation to British PM Liz Truss.",
+          "url":"https://www.indiatoday.in/world/story/uk-home-secretary-suella-braverman-sacked-quit-reports-2287393-2022-10-19",
+          "urlToImage":"https://akm-img-a-in.tosshub.com/indiatoday/images/story/202210/000_32lm4jv-sixteen_nine.jpg?VersionId=oSjMmVhLLrdAwt2E6IeHPbNsWzm_NeqH",
+          "publishedAt":"2022-10-19T16:19:51+00:00",
+          "content":"By Abhishek Chakraborty: UK home secretary Suella Braverman has quit her post amid the political and economic turmoil in Britain, saying that she had breached government rules. However, she also cite… [+2350 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":null,
+          "title":"Video: Man Turns Cooler On, Woman Loses Cool. Then This Happens",
+          "description":"The man was sleeping in the hospital's waiting hall at night with the air cooler on, when the woman arrived and switched it off. When he asked the woman why she had turned the cooler off, she lost her cool and attacked him",
+          "url":"https://www.ndtv.com/india-news/video-chhattisgarh-man-turns-cooler-on-woman-loses-cool-then-this-happens-3446806",
+          "urlToImage":"https://c.ndtvimg.com/2022-10/3fvvbt1g_slipper_625x300_19_October_22.jpg",
+          "publishedAt":"2022-10-19T16:05:39+00:00",
+          "content":"Viral video shows the woman thrashing the man with a slipper\r\nRaipur: Frayed nerves over an air cooler. That is how one could perhaps describe, in a nutshell, an ugly incident that happened at a hosp… [+1221 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":null,
+          "title":"\"Leave At The Earliest\": India To Citizens In Ukraine As War Escalates",
+          "description":"The advisory came as Russian President Vladimir Putin introduced martial law in four Ukrainian regions and residents feared a fresh assault in some parts of the country.",
+          "url":"https://www.ndtv.com/india-news/india-asks-citizens-to-urgently-leave-ukraine-as-war-escalates-3446778",
+          "urlToImage":"https://c.ndtvimg.com/2022-10/mvsitno_kyiv-drone-strikes-afp_625x300_18_October_22.jpg",
+          "publishedAt":"2022-10-19T16:05:14+00:00",
+          "content":"Ukrainian cities have also been struck in recent days by drones and missiles.\r\nNew Delhi: India on Wednesday issued an advisory asking all its citizens to avoid travelling to Ukraine, citing the \"det… [+5086 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":null,
+          "title":"After Win, Mallikarjun Kharge Wanted To Visit Sonia Gandhi. She Had A Different Plan",
+          "description":"Mallikarjun Kharge's first instinct after being declared the next Congress president was to visit Sonia Gandhi at her 10, Janpath home. He requested an appointment but did not get one.",
+          "url":"https://www.ndtv.com/india-news/should-m-kharge-visit-sonia-gandhi-or-congress-quick-change-of-plans-3445557",
+          "urlToImage":"https://c.ndtvimg.com/2022-10/93er3idk_sonia-kharge-650_625x300_19_October_22.jpg",
+          "publishedAt":"2022-10-19T15:17:00+00:00",
+          "content":"Sonia Gandhi's visit to M Kharge's home marks a break from convention in the Congress.\r\nNew Delhi: Mallikarjun Kharge's first move after being declared the next Congress president was to seek an appo… [+1720 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":"Manjiri Chitre",
+          "title":"PM to Kharge on being elected Cong president: ‘May he have a fruitful tenure’",
+          "description":"Kharge became the first non-Gandhi Congress president in 24 years, defeating Shashi Tharoor. | Latest News India",
+          "url":"https://www.hindustantimes.com/india-news/pm-modi-congratulates-mallikarjun-kharge-on-being-elected-congress-president-101666181479498.html",
+          "urlToImage":"https://images.hindustantimes.com/img/2022/10/19/1600x900/85316532-4852-11ed-ab1f-c6755d0fab71_1665377682801_1665377682801_1666181698102_1666181698102.jpg",
+          "publishedAt":"2022-10-19T12:17:49+00:00",
+          "content":"Prime Minister Narendra Modi on Wednesday congratulated Mallikarjun Kharge on being elected the Congress party president. My best wishes to Shri Mallikarjun Kharge Ji for his new responsibility as Pr… [+1641 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":null,
+          "title":"5G Tech Will Take Education In India To The Next Level: PM Modi",
+          "description":"Prime Minister Narendra Modi today said the 5G telecom service would take the education system in the country to the next level as the latest technology will go beyond \"smart facilities, smart classrooms, and smart teachings\".",
+          "url":"https://www.ndtv.com/india-news/5g-india-pm-narendra-modi-says-5g-tech-will-take-education-in-india-to-the-next-level-3445331",
+          "urlToImage":"https://c.ndtvimg.com/2022-05/pe1vf77o_pm-modi-pti-650_625x300_17_May_22.jpg",
+          "publishedAt":"2022-10-19T10:42:07+00:00",
+          "content":"PM Modi said 5G telecom service would take the education system in the country to the next level\r\nAdalaj, Gujarat: Prime Minister Narendra Modi today said the 5G telecom service would take the educat… [+3861 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":null,
+          "title":"In Mumbai, UN Chief's Strong Comments On Human Rights In India",
+          "description":"United Nations Secretary-General Antonio Guterres today criticized India over its human rights record during a three-day visit.",
+          "url":"https://www.ndtv.com/india-news/in-mumbai-un-chiefs-strong-comments-on-human-rights-in-india-3445056",
+          "urlToImage":"https://c.ndtvimg.com/2022-10/86toe44_guterres-mumbai_625x300_19_October_22.jpg",
+          "publishedAt":"2022-10-19T09:34:00+00:00",
+          "content":"UN chief Guterres also said India can \"make or break\" the 2030 Agenda for Sustainable Development.\r\nMumbai: United Nations Secretary-General Antonio Guterres today criticized India over its human rig… [+2679 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":"The Tribune India",
+          "title":"Delhi woman kidnapped, gang-raped for 2 days in Ghaziabad, ‘rod inserted in her private parts’",
+          "description":"The Delhi Commission for Women (DCW) has issued a notice to Ghaziabad police in connection with the brutal gang-rape of a 38-year-old woman recently, with the panel chief saying the attack reminded her of the Nirbhaya case.",
+          "url":"https://www.tribuneindia.com/news/nation/delhi-woman-kidnapped-gang-raped-for-2-days-in-ghaziabad-rod-inserted-in-her-private-parts-442752",
+          "urlToImage":"https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/10/2022_10$largeimg_681787291.jpg",
+          "publishedAt":"2022-10-19T06:53:00+00:00",
+          "content":"Tribune Web Desk\r\nChandigarh, October 19\r\nThe Delhi Commission for Women (DCW) has issued a notice to Ghaziabad police in connection with the brutal gang-rape of a 38-year-old woman recently, with th… [+1504 chars]"
+       },
+       {
+          "source":{
+             "id":"google-news-in",
+             "name":"Google News (India)"
+          },
+          "author":null,
+          "title":"Himachal Polls: BJP Names Candidates For 62 Seats, Drops 11 Sitting MLAs",
+          "description":"The BJP on Wednesday released its first list of 62 candidates for the polls to the 68-member Himachal Pradesh assembly, fielding Chief Minister Jai Ram Thakur from his current constituency Seraj.",
+          "url":"https://www.ndtv.com/india-news/himachal-pradesh-polls-bjp-names-candidates-for-62-seats-in-first-list-3444106",
+          "urlToImage":"https://c.ndtvimg.com/2022-06/41prtftg_bjp-generic_625x300_07_June_22.jpg",
+          "publishedAt":"2022-10-19T04:33:00+00:00",
+          "content":"Himachal Pradesh assembly polls have been scheduled for November 12. (File)\r\nNew Delhi: The BJP on Wednesday released its first list of 62 candidates for the polls to the 68-member Himachal Pradesh a… [+1869 chars]"
+       }
+    ]
+ }
